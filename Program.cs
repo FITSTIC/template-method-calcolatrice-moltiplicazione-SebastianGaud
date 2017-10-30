7cc3f996a1e330a8484e45ffb131c0b0c6161566
SebastianGaud <sebastiano.gaudeano@live.com>
2017-10-30 10:30:53 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalcolatriceTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
